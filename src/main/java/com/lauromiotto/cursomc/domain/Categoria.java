321e34cc3cf1119bffb66b383b734a80e51026d4
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-07 10:48:34 -0300

    Setting at the endpoints /categorias/{id}

--- src/main/java/com/lauromiotto/cursomc/domain/Categoria.java
+++ src/main/java/com/lauromiotto/cursomc/domain/Categoria.java
@@ -10,15 +10,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable { // significa que os objetos de categoria podem ser convertido em bits,
 													// para trafegar em redes e serem cadastrados em arquivos
 
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // definindo a geração automatica dos ID's
 	private Integer id;
 	private String nome;
+	
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
--- src/main/java/com/lauromiotto/cursomc/services/CategoriaService.java
+++ src/main/java/com/lauromiotto/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.lauromiotto.cursomc.domain.Categoria;
 import com.lauromiotto.cursomc.repositories.CategoriaRepository;
+import com.lauromiotto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
 
 }
