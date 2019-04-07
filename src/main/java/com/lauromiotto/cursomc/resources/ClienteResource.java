2a9f40af7fdf2eabbf1b477825859efcf114e6ca
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-07 12:07:18 -0300

    Endpoint /clientes/{id} available

---
+++ src/main/java/com/lauromiotto/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.lauromiotto.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lauromiotto.cursomc.domain.Cliente;
+import com.lauromiotto.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) // metodo GET por padrão para listar, para cadastrar POST, para deletar DELETE
+	// value para que se tiver um id dps de categorias na url, ele entra nesse metodo
+	public ResponseEntity<?> find(@PathVariable Integer id) { // anotação para vincular o valor q vem da url nesse id
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/lauromiotto/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.lauromiotto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lauromiotto.cursomc.domain.Cliente;
+import com.lauromiotto.cursomc.repositories.ClienteRepository;
+import com.lauromiotto.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired // automaticamente instancia a classe ClienteRepository, repo...
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", tipo: " + Cliente.class.getName()));
+	}
+
+}
