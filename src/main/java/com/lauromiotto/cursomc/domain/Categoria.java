a94880b64400f0b246188c4d4047b57b181bdecf
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-08 20:45:53 -0300

    Update: using only JsonIgnore

--- src/main/java/com/lauromiotto/cursomc/domain/Categoria.java
+++ src/main/java/com/lauromiotto/cursomc/domain/Categoria.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable { // significa que os objetos de categoria podem ser convertido em bits,
@@ -23,7 +22,6 @@ public class Categoria implements Serializable { // significa que os objetos de 
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
