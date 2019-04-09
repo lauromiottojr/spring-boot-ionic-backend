1251354b19aaccbffc84f5fcad8ed67959d8f6e4
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-09 20:56:48 -0300

    Insent new Category with POST

--- src/main/java/com/lauromiotto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lauromiotto/cursomc/resources/CategoriaResource.java
@@ -1,27 +1,40 @@
 package com.lauromiotto.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lauromiotto.cursomc.domain.Categoria;
 import com.lauromiotto.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET) // metodo GET por padrão para listar, para cadastrar POST, para deletar DELETE
-	// value para que se tiver um id dps de categorias na url, ele entra nesse metodo
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // metodo GET por padrão para listar, para cadastrar
+																	// POST, para deletar DELETE
+	// value para que se tiver um id dps de categorias na url, ele entra nesse
+	// metodo
 	public ResponseEntity<?> find(@PathVariable Integer id) { // anotação para vincular o valor q vem da url nesse id
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){ // anotação para que o json seja convertido para o obj java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // pega a url http://localhost:8080/categorias e acrescenta o id
+		return ResponseEntity.created(uri).build();
+	}
+
 }
